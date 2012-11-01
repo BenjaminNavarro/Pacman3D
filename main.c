ecec4b433cb8cb44b19f61e68035d6f351b78c89
Benjamin NAVARRO <navarro.benjamin13@gmail.com>
2012-11-01 23:33:16 +0100

    mise à jour fichier main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  *	M1 EEA- Universit� Montpellier 2
  *	2012 - 2013
  *
-*/
+ */
 
 /***				Includes					***/
 // Standard libraries
@@ -13,7 +13,6 @@
 
 // OpenGL / GLUT libraries
 #include "OpenGLHeader.h"
-
 
 // Personnal libraries
 #include "OpenGLFunctions.h"
@@ -24,7 +23,7 @@ int main(int argc, char** argv) {
 	//Initialize GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	
 	//Create the window
 	glutCreateWindow("Pacman");
@@ -36,7 +35,10 @@ int main(int argc, char** argv) {
 	glutKeyboardUpFunc(handleKeyup);
 	glutReshapeFunc(handleResize);
 	
-	glutTimerFunc(speed, PAC_Update, 0); //Add a timer
+	glutTimerFunc(speed, PAC_Update, 0); 				//Add the Pacman timer
+	glutTimerFunc(animationSpeed, PAC_Animation, 0); 	//Add the Pacman animation timer
+	glutTimerFunc(ghostSpeed, Ghost_Update, 0); 		//Add the ghosts timer
+	glutTimerFunc(20, refresh, 0);						//Add the screen refresh timer
 	
 	glutMainLoop();
 	return 0;
